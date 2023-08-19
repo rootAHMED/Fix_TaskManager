c4f05620f2506c6b5927da71108c945920bdaeb4
AHMED ALBALAWI <102583986+rootAHMED@users.noreply.github.com>
2023-08-19 15:58:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+class Program{
+    [DllImport("User32.dll", CharSet = CharSet.Unicode)]
+    public static extern int MessageBox(IntPtr h, string m, string c, int type);
+    public static void Main(string[] args){
+        RegistryKey FixTaskmgr = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+        FixTaskmgr.DeleteValue("DisableTaskMgr");
+        FixTaskmgr.Close();       
+        MessageBox((IntPtr)0, "Fix Task Manager", "succeeded", 0);        
+        Process.Start("C:\\Windows\\System32\\Taskmgr.exe");
+    }
+}
